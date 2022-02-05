1c1feec886c165b70e1963f67ca503680ef11f23
Justin Ochoa <just.och92@gmail.com>
2022-02-05 13:36:13 -0500

    React Image Slider using framer motion

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import images from "./images";
+
+function App() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>React Slider</h1>
+      <motion.div
+        ref={carousel}
+        className="carousel"
+        whileTap={{ cursor: "grabbing" }}
+      >
+        <motion.div
+          drag="x"
+          dragConstraints={{ right: 0, left: -width }}
+          className="inner-carousel"
+        >
+          {images.map((image) => {
+            return (
+              <motion.div className="item" key={image}>
+                <img src={image} alt="img" />
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,7 @@
+import image1 from "./images/pexels-hidden-couple-3048527.jpg";
+import image2 from "./images/pexels-jermaine-ulinwa-3130928.jpg";
+import image3 from "./images/pexels-jimmy-chan-2224890.jpg";
+import image4 from "./images/pexels-nilay-ramoliya-3111251.jpg";
+import image5 from "./images/pexels-samarth-singhai-1010079.jpg";
+
+export default [image1, image2, image3, image4, image5];
